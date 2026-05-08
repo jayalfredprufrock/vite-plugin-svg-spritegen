f47a2e3b722d51e42ae1b2ab234a2a4719016db7
smiley-uriux <andrews@uriux.com>
2026-05-08 12:21:05 -0400

    fix: bad release [release]

--- release.config.js
+++ release.config.js
@@ -1,15 +1,18 @@
 const config = {
-    branches: ['main'],
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      '@semantic-release/npm',
-      ["@semantic-release/git", {
-        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-      }],
-      '@semantic-release/github',
-      'semantic-release-major-tag'
-    ]
+  branches: ['main', { name: '1.x', range: '1.x' }],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
+    'semantic-release-major-tag',
+  ],
 };
 
 module.exports = config;
